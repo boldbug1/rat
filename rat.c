8ea6d90a9f2b0075f0fd7c4b8eaa794159c5bf17
boldbug <boldbug9@gmail.com>
2026-07-17 18:44:01 +0530

    fix : optimize the program and free memory allocs

--- rat.c
+++ rat.c
@@ -10,8 +10,8 @@ int createfile(const char *filename,const char *filecontent){
         return -1;
     }
     fprintf(fp,"%s",filecontent);
-    return 0;
     fclose(fp);
+    return 0;
 
 }
 
@@ -20,20 +20,16 @@ char *get_filecontent(const char *filename) {
     if (!fp) {
         return NULL;
     }
-
-   
     fseek(fp, 0, SEEK_END);
     long file_size = ftell(fp);
     rewind(fp);
 
-   
     char *content = malloc(file_size + 1);
     if (!content) {
         fclose(fp);
         return NULL; 
     }
 
-   
     size_t read_size = fread(content, sizeof(char), file_size, fp);
     content[read_size] = '\0';
 
@@ -42,30 +38,14 @@ char *get_filecontent(const char *filename) {
 }
 
 int concat(const char *filename1,const char *filename2){
-    FILE *fp1;
-    fp1 = fopen(filename1,"r");
-    if(!fp1){
+    char *filecontent1 = get_filecontent(filename1);
+    if (filecontent1 == NULL) {
+        printf("Error: getfilecontent returned an error status (-1)\n");
         return -1;
     }
-    char filecontent1[MAX];
-    size_t ret_code = fread(filecontent1,sizeof(char),MAX - 1,fp1);
-    filecontent1[ret_code] = '\0';
-
-    if(ret_code != strlen(filecontent1)){
-        printf("Error copying file 1");
-        return -1;
-    }
-    fclose(fp1);
-    FILE *fp2;
-    fp2 = fopen(filename2,"r");
-    if(!fp2){
-        return -2;
-    }
-    char filecontent2[MAX];
-    ret_code = fread(filecontent2,sizeof(char),MAX - 1,fp2);
-    filecontent2[ret_code] = '\0';
-    if(ret_code != strlen(filecontent2)){
-        printf("Error copying file 2");
+    char *filecontent2 = get_filecontent(filename2);
+    if(filecontent2 == NULL){
+        printf("Error: getfilecontent returned an error status (-1)\n");
         return -1;
     }
     size_t finalsize = strlen(filecontent1) + strlen(filecontent2);
@@ -84,6 +64,9 @@ int concat(const char *filename1,const char *filename2){
     finalfile[char_pointer] = '\0';
 
     printf("%s",finalfile);
+    free(filecontent1);
+    free(filecontent2);
+    free(finalfile);
     return 0;
     
 }
@@ -98,11 +81,10 @@ int new_line_count(const char *filecontent){
             n++;
         }
     }
-
     return n;
 }
 
-char  *numbered_out(const char *filecontent){
+char *numbered_out(const char *filecontent){
     int line_num = 1;
     int j = 0;
     int num_idx = 0;
@@ -152,38 +134,31 @@ int main(int argc , char *argv[]){
     }else if(strcmp("-n",argv[1]) == 0){
         if(argc == 3){
             const char *filename = argv[2];
-             char *filecontent = get_filecontent(filename);
+            char *filecontent = get_filecontent(filename);
              if(filecontent == NULL){
                 printf("Error : Could not read file %s\n",filename);
              }
             char *numbered_text = numbered_out(filecontent);
             printf("\n%s\n",numbered_text);
+            free(filecontent);
         }else{
             printf("Usage : rat -n <filename>");
         }
     }else{
-         const char *filename = argv[1];
-    char filecontent[MAX];
-    int i = 0;
-    int c;
-    while((c = getchar()) != EOF && i < MAX - 1){
-        filecontent[i++] = c;
-    }
-    filecontent[i] = '\0'; //string complete
-
-    if(filename){
-        if(filecontent){
-            if(createfile(filename,filecontent) == -1){
-                printf("Error while creating file");
-                return -1;
+        const char *filename = argv[1];
+        char *filecontent = get_filecontent(filename);
+        if(filename){
+            if(filecontent){
+                if(createfile(filename,filecontent) == -1){
+                    printf("Error while creating file");
+                    return -1;
+                }
             }
         }
-    }
-
-    if(strlen(filecontent) != 0){
-        printf("%s",filecontent);
-    }
-
+        if(strlen(filecontent) != 0){
+            printf("%s",filecontent);
+        }
+        free(filecontent);
     }
     
    
